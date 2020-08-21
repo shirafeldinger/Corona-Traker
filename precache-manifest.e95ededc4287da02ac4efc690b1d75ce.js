85cb17df5c70461625ef5145da3375cfc8408023
Shira Feldinger <shirafeldinger@gmail.com>
2020-08-21 18:36:33 +0300

    Updates

---
+++ precache-manifest.e95ededc4287da02ac4efc690b1d75ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b59723a0d91ecc0a80da3c46a5e1d340",
+    "url": "/Corona-Traker/index.html"
+  },
+  {
+    "revision": "db42cbadd17db119a67b",
+    "url": "/Corona-Traker/static/css/main.c618dd45.chunk.css"
+  },
+  {
+    "revision": "cc211300e076b40ad779",
+    "url": "/Corona-Traker/static/js/2.8e347935.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Corona-Traker/static/js/2.8e347935.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db42cbadd17db119a67b",
+    "url": "/Corona-Traker/static/js/main.6e79cb3f.chunk.js"
+  },
+  {
+    "revision": "362db8a3bc9a84f88b4d",
+    "url": "/Corona-Traker/static/js/runtime-main.6335a0eb.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Corona-Traker/static/media/image.d7265326.png"
+  }
+]);
